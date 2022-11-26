c56c5e9c1eebcd23dfbfbf2a8ff47c662891034c
Henry Koiti Arasaki <82346140+HenryArasaki@users.noreply.github.com>
2022-11-26 09:40:38 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ migrationsRun();
 
 const app = express();
 app.use(cors())
-app.use(express.json());
+app.use(express.json({limit: '25mb'}));
 
 app.use("/photos", express.static(uploadConfig.UPLOADS_FOLDER));
 
