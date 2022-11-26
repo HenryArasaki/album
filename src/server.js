f3f65a541c57cd86d2ef7e181d00d64e7f6b7ea1
Henry Arasaki <henry_arasaki@hotmail.com>
2022-11-25 22:09:36 -0300

    aumentando o limite

--- src/server.js
+++ src/server.js
@@ -17,8 +17,8 @@ app.use(express.json());
 
 app.use("/photos", express.static(uploadConfig.UPLOADS_FOLDER));
 
-app.use(express.json({limit: "30mb", extended: true}))
-app.use(express.urlencoded({limit: "30mb", extended: true, parameterLimit: 50000}))
+app.use(express.json())
+app.use(express.urlencoded({limit: "50mb", extended: true, parameterLimit: 100000}))
 
 app.use(routes);
 
