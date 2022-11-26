3d983a9e89c766ebefe942945a98105f1c842376
Henry Arasaki <henry_arasaki@hotmail.com>
2022-11-25 21:52:10 -0300

    agora vai

--- src/server.js
+++ src/server.js
@@ -38,6 +38,6 @@ app.use((error, req, res, next) => {
   });
 });
 
-const PORT = process.end.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is runnin on Port ${PORT}`));
